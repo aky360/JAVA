1f71716f8bcec91d4538b93923a5a3ffb5fce045
aky360 <95171943+aky360@users.noreply.github.com>
2022-04-27 16:13:22 +0530

    Create Source.java

---
+++ Source.java
@@ -0,0 +1,57 @@
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.stream.Collectors;
+
+class Validator {
+    String checkComment(String comment) throws SpamCommentException{
+        
+        String[] wordArray = {"abcde", "Imno", "pqrst", "wxyz"};
+        int count = 0;
+                         
+        String[] words=comment.split("\\s");
+        
+        var freqCaseSensitive = Arrays.stream(words)
+                         .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
+                         
+        int s0, s1, s2, s3;
+        if(freqCaseSensitive.get("abcde") != null){
+            count += freqCaseSensitive.get("abcde");
+        }
+        if(freqCaseSensitive.get("Imno") != null){
+            count += freqCaseSensitive.get("Imno");
+        }
+        if(freqCaseSensitive.get("pqrst") != null){
+            count += freqCaseSensitive.get("pqrst");
+        }
+        if(freqCaseSensitive.get("wxyz") != null){
+            count += freqCaseSensitive.get("wxyz");
+        }
+        
+                         
+        if(count > 2){
+            throw new SpamCommentException("account ban due to spam comment");
+        }
+        else{
+            throw new SpamCommentException("account ban due to spam comment");
+        }
+        
+        
+    }
+}
+
+
+class SpamCommentException extends Exception {
+    
+}
+
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        
+    }
+}
