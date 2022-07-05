8819f27211b70af5d7918ff7b8beee03ebec7b0f
aky360 <95171943+aky360@users.noreply.github.com>
2022-07-05 17:12:19 +0530

    Update ThreadExample.java

--- Threads/ThreadExample.java
+++ Threads/ThreadExample.java
@@ -30,3 +30,38 @@ public class ThreadExample extends Thread{
 Thread (Runnable r)
 In this constructor type, Runnable reference is passed and a new Thread object is created.
 */
+class Testing implements Runnable {
+    @Override
+    public void run(){
+        System.out.println("Do Something ");
+    }
+}
+
+class ThreadExample extends Thread{
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Testing());
+        t1.start();
+        System.out.println("Thread has been created with name " + t1.getName()); 
+    }
+}
+
+
+/*
+Thread (Runnable r, String r)
+We may use this constructor to generate a new Thread object by passing a Runnable reference
+as the first parameter and also providing a name for the newly generated thread.
+*/
+class Book implements Runnable {
+    @Override
+    public void run(){
+        System.out.println("Do Something task ");
+    }
+}
+
+class ThreadExample extends Thread{
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Book(), "book thread");
+        t1.start();
+        System.out.println("Thread has been created with name " + t1.getName()); 
+    }
+}
