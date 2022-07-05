fd2268862b12b46f65b19576002322b7d0d05134
aky360 <95171943+aky360@users.noreply.github.com>
2022-07-05 16:56:13 +0530

    Update ThreadPriority.java

--- Threads/ThreadPriority.java
+++ Threads/ThreadPriority.java
@@ -18,3 +18,20 @@ public class ThreadPriority extends Thread{
         //t3.start();
     }
 }
+
+
+/*
+The main thread's priority is set to 5 by default, and each child thread will have the same priority as its parent thread. 
+We have the ability to adjust the priority of any thread, whether it is the main thread or a user-defined thread. 
+It is advised to adjust the priority using the Thread class's constants, which are as follows:
+*/
+
+/*
+Thread.MIN_PRIORITY = 1;
+Thread.NORM_PRIORITY = 5;
+Thread.MAX_PRIORITY = 10;
+
+1 is known as the lowest priority.
+5 is known as standard priority.
+10 represents the highest level of priority.
+*/
