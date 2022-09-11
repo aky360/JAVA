f855fb609583d025ac4b1de66bcd80b6ae1f6f48
aky360 <95171943+aky360@users.noreply.github.com>
2022-09-11 14:53:24 +0530

    Update addBinary.java

--- LeetCode/Easy/addBinary.java
+++ LeetCode/Easy/addBinary.java
@@ -1,4 +1,15 @@
 class Solution {
+    
+    public static void main(String[] args) {
+        String a = "1010";
+        String b = "1011";
+        //System.out.println(add(a, b));
+        System.out.println(sum(a, b));
+        System.out.println('a' - '0');
+        System.out.println('1' - '0');
+        System.out.println('0' - '0');
+    }
+    
     public String addBinary(String a, String b) {
         return sum(a, b);
     }
@@ -18,5 +29,30 @@ class Solution {
             
         } 
         return (carry == 0) ? res : "1" + res; 
+    }
+}
+
+
+//another way to add binary number in string in java
+class Solution{
+    
+    public static void main(String[] args) {
+        String a = "1010";
+        String b = "1011";
+        System.out.println(add(a, b));
+        System.out.println('a' - '0');
+        System.out.println('1' - '0');
+        System.out.println('0' - '0');
+    }
+    
+    public String addBinary(String a, String b) {
+        return add(a, b);
+    }
+    
+    public static String add(String first, String second) { 
+        int b1 = Integer.parseInt(first, 2); 
+        int b2 = Integer.parseInt(second, 2); 
+        int sum = b1 + b2; 
+        return Integer.toBinaryString(sum); 
     }
 }
