d82407ccfd1bce5a5584736edfd0fb2da061ae62
aky360 <95171943+aky360@users.noreply.github.com>
2022-09-16 12:44:04 +0530

    Update SquareRoot.java

--- LeetCode/Easy/SquareRoot.java
+++ LeetCode/Easy/SquareRoot.java
@@ -3,8 +3,12 @@ class Solution {
     public static void main(String[] args) {
         Solution obj = new Solution();
         System.out.println(obj.mySqrt(100));
+        System.out.println(obj.mySqrt(1024));
+        System.out.println(obj.mySqrt(4096));
     }
     
+    
+    // USING BINARY SEARCH APPROACH 
     public int mySqrt(int x) {
         int left=1,right =x, result=0;
         while(left<right){
@@ -17,4 +21,24 @@ class Solution {
         }
         return (left==x/left)?left:left-1;
     }
+    
+    
+    // ANOTHER WAY IS ===>> USING BINARY SEARCH APPROACH 
+    public int mySqrt(int x) {
+        long l = 1, h = x;
+        while(l <= h){
+            long mid = (l+h)/2;
+            if(mid * mid == x){
+                return (int)mid;
+            }
+            else if(mid * mid < x){
+                l = mid + 1;
+            }
+            else {
+                h = mid - 1;
+            }
+        }
+        return (int)h;
+    }
+    
 }
