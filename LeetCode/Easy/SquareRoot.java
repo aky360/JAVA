e84728b4f8cc8e74d6c2edb3c8bc59a4d9d25483
aky360 <95171943+aky360@users.noreply.github.com>
2022-09-16 13:05:09 +0530

    Update SquareRoot.java

--- LeetCode/Easy/SquareRoot.java
+++ LeetCode/Easy/SquareRoot.java
@@ -5,21 +5,30 @@ class Solution {
         System.out.println(obj.mySqrt(100));
         System.out.println(obj.mySqrt(1024));
         System.out.println(obj.mySqrt(4096));
+        System.out.println(obj.mySqrt(81));
+        System.out.println(obj.mySqrt(512));
+    }
+    
+    // VERIFIED SOLUTION IN LEETCODE
+    public int mySqrt(int x) {
+        int i;
+        if(x==0) return 0;
+        if(x<4) return 1;
+        for(i=1;i<=x/2;i++)
+            if((i*i<=x)&&((i+1)*(i+1)>x)) return i;
+        return 0;
     }
     
     
-    // USING BINARY SEARCH APPROACH 
+    // ANOTHER WAY TO FIND THE SQUAREROOT OF INTEGER VALUE IS GIVEN
     public int mySqrt(int x) {
-        int left=1,right =x, result=0;
-        while(left<right){
-            int mid = left + (right - left)/2;
-            if(mid<x/mid){
-                left = mid+1;
-            }else{
-                right = mid;
-            }
-        }
-        return (left==x/left)?left:left-1;
+
+        if(x==0) return 0;
+        
+        int i=1;
+        while(i*i <= x) ++i;
+        
+        return i-1;
     }
     
     
@@ -41,15 +50,19 @@ class Solution {
         return (int)h;
     }
     
-    // ANOTHER WAY TO FIND THE SQUAREROOT OF INTEGER VALUE IS GIVEN
+    
+    // USING BINARY SEARCH APPROACH 
     public int mySqrt(int x) {
-
-        if(x==0) return 0;
-        
-        int i=1;
-        while(i*i <= x) ++i;
-        
-        return i-1;
+        int left=1,right =x, result=0;
+        while(left<right){
+            int mid = left + (right - left)/2;
+            if(mid<x/mid){
+                left = mid+1;
+            }else{
+                right = mid;
+            }
+        }
+        return (left==x/left)?left:left-1;
     }
     
 }
