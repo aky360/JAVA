9da3c8ba4bac97342e84da92914da5ca0875ad52
aky360 <95171943+aky360@users.noreply.github.com>
2022-09-16 12:52:05 +0530

    Update SquareRoot.java

--- LeetCode/Easy/SquareRoot.java
+++ LeetCode/Easy/SquareRoot.java
@@ -41,4 +41,16 @@ class Solution {
         return (int)h;
     }
     
+    // ANOTHER WAY TO FIND THE SQUAREROOT OF INTEGER VALUE IS GIVEN
+    
+    public int mySqrt(int x) {
+
+        if(x==0) return 0;
+        
+        int i=1;
+        while(i*i <= x) ++i;
+        
+        return i-1;
+    }
+    
 }
