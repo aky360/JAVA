0c2577f2f9b0c895ef46f880fb980df0b09629ea
aky360 <95171943+aky360@users.noreply.github.com>
2022-09-17 18:39:26 +0530

    Update temp.java

--- LeetCode/temp.java
+++ LeetCode/temp.java
@@ -64,3 +64,75 @@ class HelloWorld {
         return (left==x/left)?left:left-1;
     }
 }
+==========================================================================================================================================
+    
+    
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode result = new ListNode(0);
+        ListNode head = result;
+        result = result.next;
+        
+        int list1Size = Solution.listSize(list1);
+        int list2Size = Solution.listSize(list2);
+        
+        if(list1Size == 0)
+            return list2;
+
+        if(list2Size == 0)
+            return list1;
+    
+        
+        //if(list1Size > list2Size){
+            while(list1 != null && list2 != null){
+                if(list1.val >= list2.val){
+                    result.val = list2.val;
+                    result = result.next;
+                    list2 = list2.next;
+                }
+                else{
+                    result.val = list1.val;
+                    result = result.next;
+                    list1 = list1.next;
+                }
+            }
+        if(list1 != null){
+            while(list1 != null){
+                result.val = list1.val;
+                result = result.next;
+                list1 = list1.next;
+            }
+        }
+        if(list2 != null){
+            while(list2 != null){
+                result.val = list2.val;
+                result = result.next;
+                list2 = list2.next;
+            }
+        }
+        
+        //}
+        return head.next;
+    }
+    
+    
+    public static int listSize(ListNode head){
+        int count = 0;
+        ListNode temp = head;
+        while(temp != null){
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+}
