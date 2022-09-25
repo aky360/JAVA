953c1a8b2f12b0f5a22dc13c093fa31448df889e
aky360 <95171943+aky360@users.noreply.github.com>
2022-09-25 23:37:47 +0530

    Create backspaceStrCompare.java

---
+++ LeetCode/Easy/backspaceStrCompare.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return stringCompare(s).equals(stringCompare(t));
+    }
+    public String stringCompare(String s){
+        StringBuilder str = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(c != '#'){
+                str.append(c);
+            }else if(str.length() != 0){
+                str.deleteCharAt(str.length()-1);
+            }
+        }
+        return str.toString();
+    }
+}
