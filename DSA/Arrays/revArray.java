602783e792a0fa5595e1df1f70e5e9a44cd53f4e
aky360 <95171943+aky360@users.noreply.github.com>
2022-10-08 01:04:21 +0530

    Create revArray.java

---
+++ DSA/Arrays/revArray.java
@@ -0,0 +1,30 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,5,6};
+        int[] arx = arrRev(arr);
+        printArray(arx);
+    }
+    
+    public static int[] arrRev(int[] arr){
+        int n = arr.length-1;
+        int start = 0;
+        int end = arr.length-1;
+        int temp;
+        while(start < end){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        return arr;
+    }
+    
+    static void printArray(int arr[]){
+        int size = arr.length;
+        for (int i = 0; i < size; i++)
+             System.out.print(arr[i] + " ");
+          
+         System.out.println();
+    }
+}
