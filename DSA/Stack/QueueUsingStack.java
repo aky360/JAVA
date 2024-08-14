eee1fb8db868908e82bc2177e7f767acd2753f80
aky360 <95171943+aky360@users.noreply.github.com>
2024-08-14 17:18:46 +0530

    Update QueueUsingStack.java

--- DSA/Stack/QueueUsingStack.java
+++ DSA/Stack/QueueUsingStack.java
@@ -56,68 +56,72 @@ public static void main(String[] args) {
 
 
 ======================================================================================================================================================
-// Java Program to implement a queue using one stack
+/* Java Program to implement a queue using two stacks */
+// Note that Stack class is used for Stack implementation
 
 import java.util.Stack;
 
-public class QOneStack {
-	// class of queue having two stacks
+public class GFG {
+	/* class of queue having two stacks */
 	static class Queue {
 		Stack<Integer> stack1;
+		Stack<Integer> stack2;
 	}
 
 	/* Function to push an item to stack*/
-	static void push(Stack<Integer> top_ref, int new_data){
-		/* put in the data */
+	static void push(Stack<Integer> top_ref, int new_data)
+	{
+		// Push the data onto the stack
 		top_ref.push(new_data);
 	}
 
 	/* Function to pop an item from stack*/
-	static int pop(Stack<Integer> top_ref){
+	static int pop(Stack<Integer> top_ref)
+	{
 		/*If stack is empty then error */
-		if (top_ref == null) {
+		if (top_ref.isEmpty()) {
 			return -1;
 		}
-		// return element from stack
+
+		// pop the data from the stack
 		return top_ref.pop();
 	}
 
-	/* Function to enqueue an item to queue */
-	static void enQueue(Queue q, int x){
+	// Function to enqueue an item to the queue
+	static void enQueue(Queue q, int x)
+	{
 		push(q.stack1, x);
 	}
 
 	/* Function to deQueue an item from queue */
-	static int deQueue(Queue q){
-		int x, res = 0;
-		/* If the stacks is empty then error */
-		if (q.stack1.isEmpty()) {
+	static int deQueue(Queue q)
+	{
+		int x;
+
+		/* If both stacks are empty then error */
+		if (q.stack1.isEmpty() && q.stack2.isEmpty()) {
 			return -1;
 		}
-		// Check if it is a last element of stack
-		else if (q.stack1.size() == 1) {
-			return pop(q.stack1);
+
+		/* Move elements from stack1 to stack 2 only if
+		stack2 is empty */
+		if (q.stack2.isEmpty()) {
+			while (!q.stack1.isEmpty()) {
+				x = pop(q.stack1);
+				push(q.stack2, x);
+			}
 		}
-		else {
-
-			/* pop an item from the stack1 */
-			x = pop(q.stack1);
-
-			/* store the last deQueued item */
-			res = deQueue(q);
-
-			/* push everything back to stack1 */
-			push(q.stack1, x);
-			return res;
-		}
+		x = pop(q.stack2);
+		return x;
 	}
 
 	/* Driver function to test above functions */
-	public static void main(String[] args){
+	public static void main(String args[])
+	{
 		/* Create a queue with items 1 2 3*/
 		Queue q = new Queue();
 		q.stack1 = new Stack<>();
-
+		q.stack2 = new Stack<>();
 		enQueue(q, 1);
 		enQueue(q, 2);
 		enQueue(q, 3);
@@ -125,7 +129,7 @@ public class QOneStack {
 		/* Dequeue items */
 		System.out.print(deQueue(q) + " ");
 		System.out.print(deQueue(q) + " ");
-		System.out.print(deQueue(q) + " ");
+		System.out.println(deQueue(q) + " ");
 	}
 }
 
