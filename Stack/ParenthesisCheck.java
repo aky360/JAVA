6616860399c1718f600178807a28e10d3798e34f
aky360 <95171943+aky360@users.noreply.github.com>
2024-08-18 14:58:41 +0530

    Update ParenthesisCheck.java

--- Stack/ParenthesisCheck.java
+++ Stack/ParenthesisCheck.java
@@ -33,11 +33,27 @@ class Solution {
         }
         return (stack.isEmpty());
     } 
+
+    // =======================================================  OR  ===============================================================
+
+    static boolean areBracketsBalanced(String s){
+        int i=-1;
+        char stack[] = new char[s.length()];
+        for(char ch:s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='['){
+                stack[++i] = ch;
+            }
+            else{
+                if(i>=0 && ((stack[i]=='(' && ch==')') || (stack[i]=='{' && ch=='}') || (stack[i]=='[' && ch==']'))) i--;
+                else return false;
+            }
+        }
+        return (i==-1);
+    }
     
     
     public static void main(String[] args) {
         String expr = "([{}])";
-
         if (areBracketsBalanced(expr))
             System.out.println("Balanced ");
         else
